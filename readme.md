d66adda068d556c5f75fdaf6cfeba07b77a7fe2c
Riza ERGUN <re@rizaergun.com>
2019-05-03 16:49:16 +0300

    added character shorter, word shorter, word censored

--- readme.md
+++ readme.md
@@ -8,11 +8,18 @@ use RizaErgun\TextHelper\TextHelper;
 require 'vendor/autoload.php';
 
 $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, quasi.";
-$limit = 11;
+$limit = 5;
 
-$shorterText = TextHelper::shorter($text, $limit);
+$character_shorter = TextHelper::character_shorter($text, $limit);
+print_r($character_shorter);
 
-echo $shorterText;
+$word_shorter = TextHelper::word_shorter($text, $limit);
+print_r($word_shorter);
+
+$censored_words = array('ipsum', 'sit');
+$replacement = '***';
+$word_censored = TextHelper::word_censored($text, $censored_words, $replacement);
+print_r($word_censored);
 ```
 
 ## Installation
--- src/TextHelper.php
+++ src/TextHelper.php
@@ -9,15 +9,39 @@ class TextHelper
      * @param $limit
      * @return string
      */
-    public static function shorter($text, $limit)
+    public static function character_shorter($text, $limit)
     {
         if (strlen($text) > $limit) {
             $new_text = substr($text, 0, $limit);
             $new_text = trim($new_text);
             return $new_text . "..";
-        }
-        else {
+        } else {
             return $text;
         }
+    }
+
+    /**
+     * @param $text
+     * @param $limit
+     * @return string
+     */
+    public static function word_shorter($text, $limit)
+    {
+        preg_match('/^\s*+(?:\S++\s*+){1,' . (int)$limit . '}/', $text, $matches);
+
+        return rtrim($matches[0]);
+    }
+
+    /**
+     * @param $text
+     * @param $censored_words
+     * @param string $replacement
+     * @return string
+     */
+    public static function word_censored($text, $censored_words, $replacement = '')
+    {
+        $text = str_replace($censored_words, $replacement, $text);
+
+        return trim($text);
     }
 }
