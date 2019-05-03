ff79bbf8fb362132e4935973354f58eb1b35827b
Riza ERGUN <re@rizaergun.com>
2019-05-03 04:05:27 +0300

    edit example

--- examples.php
+++ examples.php
@@ -4,11 +4,9 @@ use RizaErgun\TextHelper\TextHelper;
 
 require 'vendor/autoload.php';
 
-echo "<pre>";
-
 $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, quasi.";
 $limit = 11;
-$shorterText = TextHelper::shorter($text, $limit);
-print_r($shorterText);
 
-echo "</pre>";
+$shorterText = TextHelper::shorter($text, $limit);
+
+echo $shorterText;
